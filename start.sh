5b3a2eccb8dd52f08f71d3d26a9fb9153edec51b
Tonystarkx7x <146725814+Tonystarkx7x@users.noreply.github.com>
2023-10-10 18:25:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tonystarkx7x/Doctor_stranger
+  git clone https://github.com/Tonystarkx7x/Doctor_stranger /Doctor_stranger
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Doctor_stranger
