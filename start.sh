a087d6a3e2bdfdb84b4f2ae18c471a246040bccc
Tonystarkx7x <146725814+Tonystarkx7x@users.noreply.github.com>
2023-10-10 18:21:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tonystarkx7x/tony_stark /tony_stark
+  git clone https://github.com/Tonystarkx7x/Doctor_stranger
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /tony_stark
+  git clone $UPSTREAM_REPO /Doctor_stranger
 fi
-cd /Advance-Auto-Filter
+cd /Doctor_stranger
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
